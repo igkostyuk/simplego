50642a54c79f74ae17c53776dc6fa014fcd3abf1
Ig Kos <ig.fire@gmail.com>
2022-06-28 16:58:02 +0300

    feat: add remote state

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,5 +1,12 @@
 
 terraform {
+  backend "s3" {
+    bucket         = "terraforms-states-dev"
+    key            = "appgo/terraform.tfstate"
+    encrypt        = true
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+  }
   required_providers {
     aws = {
       version = "~> 4.20"
