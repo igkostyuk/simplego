a4180c0660fa3853fd4e19568d0d815d3952d743
Ig Kos <ig.fire@gmail.com>
2022-07-11 16:35:19 +0300

    feat: test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,7 +18,7 @@ variable "environment" {
 variable "az_count" {
   description = "Number of AZs to cover in a given region"
   type        = number
-  default     = 2
+  default     = 3
 }
 
 variable "app_name" {
